ada7e74f534bcffd2328e7a114d118a8a4c4009c
Moon Chang <15237182+zmyxpt@users.noreply.github.com>
2025-03-18 12:28:45 +0800

    Update auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -6,10 +6,12 @@ cd "$HOME"/debian-ss-main
 
 apt-get update
 apt-get upgrade --with-new-pkgs -y
+apt-get autoremove --purge -y
 
 docker-compose down
 docker-compose pull
 docker-compose build --no-cache --pull
 docker-compose up -d
+docker system prune --volumes -f
 
 systemctl reboot
